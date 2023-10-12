02d4afddec85fe23387833e299edc8cc3f8797d4
kristennakamura <kristennakamura@Kristens-MacBook-Air.local>
2023-10-12 13:49:30 -0400

    moved App and added initial structure

---
+++ src/components/App/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.scss';
+
+// import SearchBar from '../SearchBar/SearchBar';
+// import SearchResults from '../SearchResults/SearchResults';
+// import Playlist from '../Playlist/Playlist';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-heading">Ja<span>mmm</span>ing</h1>
+      </header>
+      {/* <SearchBar/> */}
+      <div className="App-columnWrapper">
+        <div className="App-column">
+          {/* <SearchResults/> */}
+        </div>
+        <div className="App-column">
+          {/* <Playlist/> */}
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
